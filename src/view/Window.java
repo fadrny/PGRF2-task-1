3a8e78ca1932fd9d4f133a879fddba94d571b026
fadrny <marin.fadrny@gmail.com>
2026-05-01 23:58:37 +0200

    feat: Update window title for PGRF2 task 1

--- src/view/Window.java
+++ src/view/Window.java
@@ -9,7 +9,7 @@ public class Window extends JFrame {
 
     public Window(int width, int heigth) {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("PGRF1 2024/2025");
+        setTitle("PGRF2 - task 1 - Marek Fadrný");
         setVisible(true);
 
         panel = new Panel(width, heigth);
